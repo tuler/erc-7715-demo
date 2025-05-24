3b75bf1e8035ea4035b27a5c3ea4776f059eed3b
Danilo Tuler <tuler@pobox.com>
2025-05-24 00:16:33 -0400

    pnpm create wagmi

---
+++ README.md
@@ -0,0 +1,5 @@
+# ERC-7715 Demo
+
+This demonstrates the usage of [ERC-7715](https://eip.tools/eip/7715), [EIP-5792](https://eip.tools/eip/5792) and [ERC-4337](https://eip.tools/eip/4337) with a [Cartesi](https://cartesi.io) application.
+
+The implementation uses [ZeroDev Kernel](https://docs.zerodev.app) as Smart Account.
---
+++ src/wagmi.ts
@@ -0,0 +1,23 @@
+import { cookieStorage, createConfig, createStorage, http } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { injected } from "wagmi/connectors";
+
+export function getConfig() {
+    return createConfig({
+        chains: [sepolia],
+        connectors: [injected()],
+        storage: createStorage({
+            storage: cookieStorage,
+        }),
+        ssr: true,
+        transports: {
+            [sepolia.id]: http(),
+        },
+    });
+}
+
+declare module "wagmi" {
+    interface Register {
+        config: ReturnType<typeof getConfig>;
+    }
+}
